01c4d15b040aaebaeea19eb537a186458d23a682
intentmoose <cariel226@gmail.com>
2021-12-27 09:31:18 +0200

    init backend

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+var axios = require("axios");
+var cors = require("cors");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 3000;
+let production = process.env.NODE_ENV === "production" ? true : false;
+if (!production) {
+  var cors = require("cors"); // ⚠️⚠️ remove later⚠️⚠️
+  app.use(cors());
+}
+
+
+var token;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require("./routes")(app);
+
+app.get("/test", (req, res) => {
+  res.send("Successful response.");
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
